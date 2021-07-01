9e3591fcec5cfe36ca7c4f6041ba35d970b0cc58
Da Xue <da@libre.computer>
2021-06-30 21:56:00 -0400

    lxc builder

---
+++ build.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+if [ "$USER" != "root" ]; then
+	echo "This script must run as root!"
+	exit 1
+fi
+
+set -e
+
+cd /
+
+if [ ! -f "build_config.sh" ]; then
+	echo "Build configuration missing!"
+	exit 1
+fi
+
+. build_config.sh
+
+if [ ! -f "$SDK_FILE" ]; then
+	echo "SDK file missing!"
+	exit 1
+fi
+
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y lbzip2 ninja-build python bison build-essential zip gcc-multilib u-boot-tools m4 git-lfs gcc-arm-none-eabi lib32z1
+
+if [ ! -d "$SDK_DIR" ]; then
+	tar -I lbzip2 -xf $SDK_FILE -C /opt
+fi
+
+if [ ! -d "/opt/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux" ]; then
+	wget -O - https://releases.linaro.org/archive/13.11/components/toolchain/binaries/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux.tar.xz | tar -xJC /opt
+fi
+
+cd $SDK_DIR
+
+#PULL GITHUB CHANGES TO SDK
+
+cd bootloader/uboot-repo/bl33
+if ! git remote show lc-github; then
+git remote add lc-github https://github.com/libre-computer-project/amlogic-bootloader-uboot-repo-bl33.git
+git fetch lc-github $BRANCH
+git rebase FETCH_HEAD
+fi
+cd ../../..
+
+cd common
+if ! git remote show lc-github; then
+git remote add lc-github https://github.com/libre-computer-project/amlogic-common.git
+git fetch lc-github android-p-amlogic-20190415-aosp
+git rebase FETCH_HEAD
+fi
+cd ..
+
+cd device/amlogic
+if ! git remote show lc-github; then
+git remote add lc-github https://github.com/libre-computer-project/amlogic-device-amlogic.git
+git fetch lc-github android-p-amlogic-20190415-aosp
+git rebase FETCH_HEAD
+fi
+cd ../..
+
+cd vendor/amlogic/common/apps/DroidTvSettings
+if ! git remote show lc-github; then
+git remote add lc-github https://github.com/libre-computer-project/amlogic-vendor-amlogic-common-apps-DroidTvSettings.git
+git fetch lc-github android-p-amlogic-20190415-aosp
+git rebase FETCH_HEAD
+fi
+cd ../../../../..
+
+#DEVICE ID APK
+
+if [ ! -d "$SDK_DIR/packages/apps/DeviceID" ]; then
+mkdir -p $SDK_DIR/packages/apps/DeviceID
+cp /$DEVICEID_FILE $SDK_DIR/packages/apps/DeviceID/deviceid.apk
+tee $SDK_DIR/packages/apps/DeviceID/Android.mk <<EOF
+LOCAL_PATH := \$(call my-dir)
+include \$(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := DeviceID
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := deviceid.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+include \$(BUILD_PREBUILT)
+EOF
+fi
+
+#OPENGAPPS
+
+mkdir -p vendor/opengapps/sources
+if [ ! -d vendor/opengapps/build/.git ]; then
+git clone https://github.com/opengapps/aosp_build vendor/opengapps/build
+cd vendor/opengapps/build
+git lfs pull
+cd ../../..
+fi
+if [ ! -d vendor/opengapps/sources/all/.git ]; then
+git clone https://gitlab.opengapps.org/opengapps/all --depth=1 --single-branch vendor/opengapps/sources/all
+cd vendor/opengapps/sources/all
+git lfs pull
+cd ../../../..
+fi
+if [ ! -d vendor/opengapps/sources/arm/.git ]; then
+git clone https://gitlab.opengapps.org/opengapps/arm --depth=1 --single-branch vendor/opengapps/sources/arm
+cd vendor/opengapps/sources/arm
+git lfs pull
+cd ../../../..
+fi
+
+#U-BOOT
+if [ ! -d "/opt/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf" ]; then
+	wget -O - https://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/arm-linux-gnueabihf/gcc-linaro-6.3.1-2017.02-x86_64_arm-linux-gnueabihf.tar.xz | tar -xJC /opt
+fi
+if [ ! -d "/opt/gcc-arm-none-eabi-6-2017-q2-update" ]; then
+	wget -O - https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2 | tar -xjC /opt
+fi
+
+cd bootloader/uboot-repo
+
+./mk $BOARD_DT_NAME --systemroot
+
+cp build/* ../../device/amlogic/$BOARD_AMLOGIC_NAME/upgrade/
+cp build/u-boot.bin ../../device/amlogic/$BOARD_AMLOGIC_NAME/bootloader.img
+
+cd ../..
+
+#BUILD ANDROID
+
+. build/envsetup.sh
+
+lunch $BOARD_AMLOGIC_NAME-$TARGET
+
+make otapackage
---
+++ setup.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Copyright 2021 Da Xue
+
+if [ "$USER" = "root" ]; then
+	echo "This script should not run as root!"
+	exit 1
+fi
+
+if [ $# -ne 4 ]; then
+	echo "$0 CONTAINER BOARD BRANCH TARGET"
+	exit 1
+fi
+
+CONTAINER=$1
+BOARD=$2
+BRANCH=$3
+TARGET=$4
+
+SDK_DIR=/opt/release_p_9.0_20190415_aosp
+SDK_FILE=release_p_9.0_20190415_aosp.tar.bz2
+if [ ! -f "$SDK_FILE" ]; then
+	echo "$SDK_FILE does not exist!"
+	exit 1
+fi
+
+DEVICEID_FILE=deviceid.apk
+if [ ! -f "$DEVICEID_FILE" ]; then
+	echo "$DEVICEID_FILE does not exist!"
+	exit 1
+fi
+
+CONTAINERS=$(lxc list -f csv | cut -f 1 -d ,)
+container_found=0
+for container in $CONTAINERS; do
+	if [ "$CONTAINER" = "$container" ]; then
+		if [ "$CONTAINER_EXISTING" -eq 1 ]; then
+			container_found=1
+			break
+		else
+			echo "$CONTAINER already exists!"
+			exit 1
+		fi
+	fi
+done
+
+if [ "$CONTAINER_EXISTING" -eq 1 ]; then
+	if [ "$container_found" -eq 0 ]; then
+		echo "$CONTAINER does not exist!"
+		exit 1
+	fi
+fi
+
+BOARDS=(aml-s805x-ac aml-s905x-cc)
+
+board_found=0
+for board in ${BOARDS[@]}; do
+	if [ "$BOARD" = "$board" ]; then
+		board_found=1
+		break
+	fi
+done
+
+if [ "$board_found" -eq 0 ]; then
+	echo "$BOARD is not supported!"
+	exit 1
+fi
+
+declare -A BOARD_AMLOGIC_NAMES
+BOARD_AMLOGIC_NAMES[aml-s805x-ac]="curie"
+BOARD_AMLOGIC_NAMES[aml-s905x-cc]="ampere"
+
+declare -A BOARD_DT_NAMES
+BOARD_DT_NAMES[aml-s805x-ac]="gxl_p241_v1"
+BOARD_DT_NAMES[aml-s905x-cc]="gxl_p212_v1"
+
+declare -A BRANCHES
+BRANCHES[aml-s805x-ac]="android-p-amlogic-20190415-aosp"
+BRANCHES[aml-s905x-cc]="android-p-amlogic-20190415-aosp android-p-amlogic-20190415-aosp-sd"
+
+branch_found=0
+for branch in ${BRANCHES[$BOARD]}; do
+	if [ "$BRANCH" = "$branch" ]; then
+		branch_found=1
+		break
+	fi
+done
+
+if [ "$branch_found" -eq 0 ]; then
+	echo "$BRANCH is not supported!"
+	exit 1
+fi
+
+declare -A TARGETS
+TARGETS[aml-s805x-ac]="user userdebug eng"
+TARGETS[aml-s905x-cc]="user userdebug eng"
+
+target_found=0
+for target in ${TARGETS[$BOARD]}; do
+	if [ "$TARGET" = "$target" ]; then
+		target_found=1
+		break
+	fi
+done
+
+if [ "$CONTAINER_EXISTING" -eq 1 ]; then
+	echo "Re-using existing container."
+else
+	lxc launch ubuntu:18.04 $CONTAINER
+	lxc file push $SDK_FILE $CONTAINER/
+fi
+lxc file push $DEVICEID_FILE $CONTAINER/
+
+lxc file push build.sh $CONTAINER/
+
+CONFIG_FILE=$(mktemp)
+
+echo "BOARD=$BOARD" >> $CONFIG_FILE
+echo "BOARD_AMLOGIC_NAME=${BOARD_AMLOGIC_NAMES[$BOARD]}" >> $CONFIG_FILE
+echo "BOARD_DT_NAME=${BOARD_DT_NAMES[$BOARD]}" >> $CONFIG_FILE
+echo "BRANCH=$BRANCH" >> $CONFIG_FILE
+echo "TARGET=$TARGET" >> $CONFIG_FILE
+echo "SDK_DIR=$SDK_DIR" >> $CONFIG_FILE
+echo "SDK_FILE=$SDK_FILE" >> $CONFIG_FILE
+echo "DEVICEID_FILE=$DEVICEID_FILE" >> $CONFIG_FILE
+
+lxc file push $CONFIG_FILE $CONTAINER/build_config.sh
+
+lxc exec $CONTAINER /build.sh
+
+lxc file pull $CONTAINER$SDK_DIR/out/target/product/$BOARD_AMLOGIC_NAME/aml_upgrade_package.img $board.img
